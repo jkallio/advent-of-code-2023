970ce2fbcb2f2f637ad4258097ef1f398d2e87d3
Jussi Kallio <jkallio@gmail.com>
2023-12-03 11:47:19 +0200

    Solved 01-2

--- 01/init.lua
+++ 01/init.lua
@@ -5,24 +5,37 @@ if file == nil then
     return
 end
 
+local search_strings = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero" }
 local sum = 0
 for line in file:lines() do
     local first_digit
     local last_digit
-    for c in line:gmatch("%d") do
-        if first_digit == nil then
-            first_digit = c
+
+    for i = 1, #line do
+        local digit = line:sub(i, i)
+        local num = tonumber(digit)
+        if num == nil then
+            for j, search_num in ipairs(search_strings) do
+                local substr = line:sub(i, i + #search_num - 1)
+                if substr == search_num then
+                    digit = tostring(j)
+                    break
+                end
+            end
+            num = tonumber(digit)
         end
-        last_digit = c
+
+        if num ~= nil then
+            if first_digit == nil then
+                first_digit = digit
+            end
+            last_digit = digit
+        end
     end
+
     local value = tonumber(first_digit .. last_digit)
-    if value == nil then
-        print("Error converting to number")
-        file:close()
-        return
-    end
     sum = sum + value
 end
-print(sum)
 
+print(sum)
 file:close()
